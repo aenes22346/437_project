e8303fe580a24fada975a1356790a57a46095d09
Ahmet Enes <aenes@sabanciuniv.edu>
2022-12-13 21:00:27 +0300

    db connection

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb+srv://437_project:sifre437sifre@cluster0.i2wjyyr.mongodb.net/test"
+
+
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 @app.route("/")
 def index():
