b3db9a6ac1a604fe38ecb06e37e6d7f2652cc571
Ahmet Enes <aenes@sabanciuniv.edu>
2022-12-20 01:31:54 +0300

    register done

--- app.py
+++ app.py
@@ -1,22 +1,69 @@
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt 
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb+srv://437_project:sifre437sifre@cluster0.i2wjyyr.mongodb.net/test"
+def get_database():
+
+    CONNECTION_STRING = "mongodb+srv://437_project:sifre437sifre@cluster0.i2wjyyr.mongodb.net/test"
+
+    client = MongoClient(CONNECTION_STRING)
 
 
-mongodb_client = PyMongo(app)
-db = mongodb_client.db
+    return client['db']
 
-@app.route("/signin")
-def login():
+
+db = get_database()
+
+collection_name = db["users"]
+
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+@app.route("/signin", methods=['GET'])
+def login_get():
+    return render_template('signin.html')
+
+@app.route("/signin", methods=['POST'])
+def login_post():
     return render_template('signin.html')
 
 
-@app.route("/signup")
-def register():
+@app.route("/signup", methods=['GET'])
+def signup_get():
     return render_template('signup.html')
+
+
+@app.route("/signup", methods=['POST'])
+def signup_post():
+
+    username = request.get_json()['username']
+    name = request.get_json()['name']
+    surname = request.get_json()['surname']
+    password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
+
+
+    user_found = collection_name.find_one({"username": username})
+
+    if user_found == None:
+
+
+        user_input = {'username': username, 'name': name, 'password': password, 'surname': surname}
+
+
+        collection_name.insert_one(user_input)
+
+
+
+        return jsonify(username + " registered")
+
+    else:
+
+
+        return jsonify("There is already a user with: " + username)
 
 
 
